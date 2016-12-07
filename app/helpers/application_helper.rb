19b78171cee1ca88bb336c438caab5117272791a
Tayler Phillips <tphillips@infotech.com>
2016-12-06 21:31:11 -0500

    Renamed the app for it's new purpose.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def page_title(text = nil)
     content_for :title do
-      "BaseApp#{': ' + text if text}"
+      "SecretSantaApp#{': ' + text if text}"
     end
   end
 end
