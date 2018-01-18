4847f4cc54c5971b68457aeb040ab515333cb97d
Tayler Phillips <tphillips@infotech.com>
2018-01-17 19:59:02 -0500

    Fix rubocop yml. Fix the datepicker not showing up.

--- app/assets/javascripts/date_time_picker.js
+++ app/assets/javascripts/date_time_picker.js
@@ -1,7 +1,13 @@
-$(document).on('turbolinks:load', function() {
+var load_date_time_picker = function() {
   $('.form_datetime').datetimepicker({
     format: 'YYYY-MM-D ha',
     showClear: true,
     ignoreReadonly: true
   });
+  $('body').on('focus', '.form_datetime', function() {
+    $(this).data('DateTimePicker').show();
+  });
+};
+$(document).on('turbolinks:load', function() {
+  load_date_time_picker();
 });
